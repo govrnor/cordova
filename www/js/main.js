6a2c14f0bcbac25bb5189d5ab900c8a548a40567
Andre Christoga <andrechristoga@gmail.com>
2016-07-26 20:48:33 +0700

    PhotoBrowser coming right up!

---
+++ www/js/main.js
@@ -0,0 +1,8 @@
+var app = new Framework7();
+
+function evolusi(nama) {
+	var myPhotoBrowser = app.photoBrowser({
+	    zoom: 400,
+	    photos: ['path/to/image1.jpg', 'path/to/image2.jpg']
+	}); 
+}
