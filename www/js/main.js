7b56095a6e4216ed3d4b0e05180ef819355c7940
Andre Christoga <andrechristoga@gmail.com>
2016-07-26 20:52:56 +0700

    It is done.

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,12 @@
 var app = new Framework7();
 
 function evolusi(nama) {
+	var folder = '../img/evolusi' + String(nama);
+	var dulu = folder + 'dulu.jpg';
+	var sekarang = folder + 'sekarang.jpg';
+
 	var myPhotoBrowser = app.photoBrowser({
 	    zoom: 400,
-	    photos: ['path/to/image1.jpg', 'path/to/image2.jpg']
+	    photos: [dulu, sekarang]
 	}); 
 }
